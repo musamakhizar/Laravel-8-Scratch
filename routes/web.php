f90f40a32ec0c2587a9510eb36ff99ba3aa31ac2
MUsamaKhizar <usamakhizar07@gmail.com>
2021-10-29 16:11:43 +0500

    Caching & Cleaning Code

--- routes/web.php
+++ routes/web.php
@@ -21,20 +21,20 @@ Route::get('/', function () {
 
 Route::get('/post/{post}', function ($slug) {
     
-    //-- The path of the dir containing files --//
-
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
-
+    
     //-- Security check file exists by passing the file path and calling file_exists function --//
-    if(! file_exists($path))
+    if(! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) //he path of the dir containing files
     {
-        //dd("Not exists");
-        //abort(404);
         return redirect("/"); // -> if post (file) does'nt exists then redirect back to the home page 
     }
-    //dd("File exits");
+    //-- Caching File --//
+    
+    $post = cache()->remember("post.{$slug}", now()->addMinute(60), function () use ($path){
+        return file_get_contents($path); //read file into a string from the given path 
+    });
 
-    $post = file_get_contents($path); //read file into a string from the given path
+    //-- short closure or arrow function --//
+    // $post = cache()->remember('post.{$slug}', now()->addMinute(60), fn() => file_get_contents($path));
 
     return view('post', compact('post'));
 })->where('post','[A-z0-9\_-]+'); //constraint
